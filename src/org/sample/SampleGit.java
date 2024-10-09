84151daab784755169abeb1862fa08a0a5c4967e
Arafa9785 <mohamadyasar.7863@gmail.com>
2024-10-09 17:55:11 +0530

    BworkDOne

--- src/org/sample/SampleGit.java
+++ src/org/sample/SampleGit.java
@@ -5,5 +5,9 @@ public class SampleGit {
 	public static void main(String[] args) {
 		
 		System.out.println("Git Project Started");
+		String CourseName = "SQL";
+		System.out.println(CourseName);
+		int reg = 5678; 
+		System.out.println(reg);
 	}
 }
